8149a07fd890340cf44eeaae43f83eb74c78a705
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2015-09-11 15:04:13 +0430

    New Images and New SQL script created

--- storage/db/adraaf(old).sql
+++ storage/db/adraaf(old).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2015 at 12:32 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Sep 10, 2015 at 09:04 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `adraaf`
 --
+CREATE DATABASE IF NOT EXISTS `adraaf` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `adraaf`;
 
 -- --------------------------------------------------------
 
@@ -27,10 +29,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `actions` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `point` int(11) NOT NULL,
-  `title` varchar(40) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `title` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `actions`
@@ -51,9 +54,10 @@ INSERT INTO `actions` (`id`, `point`, `title`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(10) unsigned NOT NULL,
-  `title` varchar(40) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `categories`
@@ -73,10 +77,11 @@ INSERT INTO `categories` (`id`, `title`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `levels` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `criteria` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `criteria` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `levels`
@@ -136,15 +141,17 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `offers` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `start_date` datetime NOT NULL,
   `end_date` datetime NOT NULL,
   `maximum_percentage` double NOT NULL,
   `store_id` int(10) unsigned NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `offers_store_id_foreign` (`store_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `offers`
@@ -163,7 +170,7 @@ INSERT INTO `offers` (`id`, `description`, `start_date`, `end_date`, `maximum_pe
 --
 
 CREATE TABLE IF NOT EXISTS `stores` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
@@ -173,18 +180,21 @@ CREATE TABLE IF NOT EXISTS `stores` (
   `y` double NOT NULL,
   `category_id` int(10) unsigned NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `stores_email_unique` (`email`),
+  KEY `stores_category_id_foreign` (`category_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `stores`
 --
 
 INSERT INTO `stores` (`id`, `email`, `password`, `title`, `address`, `img_url`, `x`, `y`, `category_id`, `created_at`, `updated_at`) VALUES
-(1, 'info@hilandbeauty.com', '12345678', 'فروشگاه عطر هایلند', 'تهران، شهرک قدس، ابتدای بلوار فرحزادی، مجتمع تجاری میلاد نور، طبقه ششم', '1.png', 35.753497, 51.362583, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'golestan@nikekish.ir', '12345678', 'فروشگاه نایک', 'شهرک غرب، ابتدای خیابان ایران زمین، مرکز تجاری گلستان', '2.jpg', 35.753427, 51.374265, 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(3, 'info@starbucks.ir', '12345678', 'کافه استارباکس', 'شهرک غرب، ابتدای خیابان ایران زمین، روبروی مهستان، مرکز تجاری ایران زمین', '3.png', 35.752774, 51.373385, 3, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(4, 'info@dahe60.ir', '12345678', 'کافه دهه شصت', 'شهرک غرب، ابتدای خیابان ایران زمین، نرسیده به مهستان، پلاک 40', '4.jpg', 35.75327, 51.371937, 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 'info@hilandbeauty.com', '12345678', 'فروشگاه عطر هایلند', 'تهران، شهرک قدس، ابتدای بلوار فرحزادی، مجتمع تجاری میلاد نور، طبقه ششم', NULL, 35.753497, 51.362583, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'golestan@nikekish.ir', '12345678', 'فروشگاه نایک', 'شهرک غرب، ابتدای خیابان ایران زمین، مرکز تجاری گلستان', NULL, 35.753427, 51.374265, 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'iranzamin@langine.ir', '12345678', 'قنادی لانجین', 'شهرک غرب، ابتدای خیابان ایران زمین، روبروی مهستان، مرکز تجاری ایران زمین', NULL, 35.752774, 51.373385, 3, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'info@dahe60.ir', '12345678', 'کافه دهه شصت', 'شهرک غرب، ابتدای خیابان ایران زمین، نرسیده به مهستان، پلاک 40', NULL, 35.75327, 51.371937, 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -193,13 +203,16 @@ INSERT INTO `stores` (`id`, `email`, `password`, `title`, `address`, `img_url`, 
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `uuid` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  UNIQUE KEY `users_uuid_unique` (`uuid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `users`
@@ -227,7 +240,10 @@ CREATE TABLE IF NOT EXISTS `user_actions` (
   `action_x` double DEFAULT NULL,
   `action_y` double DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `user_actions_user_id_foreign` (`user_id`),
+  KEY `user_actions_action_id_foreign` (`action_id`),
+  KEY `user_actions_offer_id_foreign` (`offer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -286,7 +302,9 @@ CREATE TABLE IF NOT EXISTS `user_levels` (
   `user_id` int(10) unsigned NOT NULL,
   `level_id` int(10) unsigned NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `user_levels_user_id_foreign` (`user_id`),
+  KEY `user_levels_level_id_foreign` (`level_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -305,92 +323,6 @@ INSERT INTO `user_levels` (`user_id`, `level_id`, `created_at`, `updated_at`) VA
 (5, 2, '2015-09-03 19:30:00', '0000-00-00 00:00:00');
 
 --
--- Indexes for dumped tables
---
-
---
--- Indexes for table `actions`
---
-ALTER TABLE `actions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `levels`
---
-ALTER TABLE `levels`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `offers`
---
-ALTER TABLE `offers`
-  ADD PRIMARY KEY (`id`), ADD KEY `offers_store_id_foreign` (`store_id`);
-
---
--- Indexes for table `stores`
---
-ALTER TABLE `stores`
-  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `stores_email_unique` (`email`), ADD KEY `stores_category_id_foreign` (`category_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `users_email_unique` (`email`), ADD UNIQUE KEY `users_uuid_unique` (`uuid`);
-
---
--- Indexes for table `user_actions`
---
-ALTER TABLE `user_actions`
-  ADD KEY `user_actions_user_id_foreign` (`user_id`), ADD KEY `user_actions_action_id_foreign` (`action_id`), ADD KEY `user_actions_offer_id_foreign` (`offer_id`);
-
---
--- Indexes for table `user_levels`
---
-ALTER TABLE `user_levels`
-  ADD KEY `user_levels_user_id_foreign` (`user_id`), ADD KEY `user_levels_level_id_foreign` (`level_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `actions`
---
-ALTER TABLE `actions`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `levels`
---
-ALTER TABLE `levels`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
---
--- AUTO_INCREMENT for table `offers`
---
-ALTER TABLE `offers`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `stores`
---
-ALTER TABLE `stores`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
 -- Constraints for dumped tables
 --
 
@@ -398,28 +330,28 @@ ALTER TABLE `users`
 -- Constraints for table `offers`
 --
 ALTER TABLE `offers`
-ADD CONSTRAINT `offers_store_id_foreign` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`);
+  ADD CONSTRAINT `offers_store_id_foreign` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`);
 
 --
 -- Constraints for table `stores`
 --
 ALTER TABLE `stores`
-ADD CONSTRAINT `stores_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+  ADD CONSTRAINT `stores_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 
 --
 -- Constraints for table `user_actions`
 --
 ALTER TABLE `user_actions`
-ADD CONSTRAINT `user_actions_action_id_foreign` FOREIGN KEY (`action_id`) REFERENCES `actions` (`id`),
-ADD CONSTRAINT `user_actions_offer_id_foreign` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`),
-ADD CONSTRAINT `user_actions_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `user_actions_action_id_foreign` FOREIGN KEY (`action_id`) REFERENCES `actions` (`id`),
+  ADD CONSTRAINT `user_actions_offer_id_foreign` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`),
+  ADD CONSTRAINT `user_actions_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `user_levels`
 --
 ALTER TABLE `user_levels`
-ADD CONSTRAINT `user_levels_level_id_foreign` FOREIGN KEY (`level_id`) REFERENCES `levels` (`id`),
-ADD CONSTRAINT `user_levels_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `user_levels_level_id_foreign` FOREIGN KEY (`level_id`) REFERENCES `levels` (`id`),
+  ADD CONSTRAINT `user_levels_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
